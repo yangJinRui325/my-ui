b319c936a880a593655e1f3cff004895646ce97e
yangjinrui325 <2430960358@qq.com>
2021-02-26 17:53:03 +0800

    'tree组件'

--- src/components/display/tree/sw-tree.vue
+++ src/components/display/tree/sw-tree.vue
@@ -1,7 +1,11 @@
 <template>
     <el-tree
         ref="elTree"
-        :class="{'tree-line':showLine}"
+        :class="{
+            'tree-line': showLine,
+            'high-light-icon': showSelectIcon
+        }"
+        :style="{fontSize: realFontSize +'px'}"
         :data="data"
         :emptyText="emptyText"
         :renderAfterExpand="renderAfterExpand"
@@ -27,6 +31,19 @@
         :accordion="accordion"
         :indent="realIndent"
         :iconClass="iconClass"
+        @node-click="nodeClick"
+        @node-contextmenu="nodeContextmenu"
+        @check-change="checkChange"
+        @check="check"
+        @current-change="currentChange"
+        @node-expand="nodeExpand"
+        @node-collapse="nodeCollapse"
+        @node-drag-start="nodeDragStart"
+        @node-drag-enter="nodeDragEnter"
+        @node-drag-leave="nodeDragLeave"
+        @node-drag-over="nodeDragOver"
+        @node-drag-end="nodeDragEnd"
+        @node-drop="nodeDrop"
     >
         <span class="custom-tree-node" slot-scope="{ node,data }">
             <slot :node="node" :data="data">
@@ -48,6 +65,15 @@ export default {
         showLine: {
             type: Boolean,
             default: true
+        },
+        /*是否展示选中的图标*/
+        showSelectIcon: {
+            type: Boolean,
+            default: true
+        },
+        fontSize: {
+            type: [String, Number],
+            default: 16
         },
         hight: {
             type: [String, Number],
@@ -124,10 +150,13 @@ export default {
     },
     computed: {
         realIndent() {
-            return this.showLine ? 0 : this.indent
+            return this.showLine ? 30 : this.indent
         },
         realHeight() {
             return parseToNum(this.height)
+        },
+        realFontSize() {
+            return parseToNum(this.fontSize)
         }
     },
     watch: {},
@@ -135,19 +164,80 @@ export default {
 
     },
     mounted: function () {
-        console.log(document.getElementsByClassName('el-tree-node__content'))
-        console.log(this.$refs.elTree.$el.children[0].children)
-        // let children = this.$refs.elTree.$el.children
+
     },
     methods: {
-        handleNodeClick(data) {
-            console.log(data);
+        /*
+        * */
+        nodeClick(data, node, vnode) {
+            this.$emit('node-click', data, node, vnode)
         },
+        nodeContextmenu(event, data, node, vnode) {
+            this.$emit('node-contextmenu', event, data, node, vnode)
+        },
+        checkChange(data, checked, indeterminate) {
+            this.$emit('check-change', data, checked, indeterminate)
+        },
+        check(data, checked) {
+            this.$emit('check', data, checked)
+        },
+        currentChange(data, node) {
+            this.$emit('current-change', data, node)
+        },
+        nodeExpand(data, node, vnode) {
+            this.$emit('node-expand', data, node, vnode)
+        },
+        nodeCollapse(data, node, vnode) {
+            this.$emit('node-collapse', data, node, vnode)
+        },
+        nodeDragStart(node, event) {
+            this.$emit('node-drag-start', node, event)
+        },
+        nodeDragEnter(dragNode, enterNode, event) {
+            this.$emit('node-drag-enter', dragNode, enterNode, event)
+        },
+        nodeDragLeave(dragNode, leaveNode, event) {
+            this.$emit('node-drag-leave', dragNode, leaveNode, event)
+        },
+        nodeDragOver(dragNode, overNode, event) {
+            this.$emit('node-drag-over', dragNode, overNode, event)
+        },
+        nodeDragEnd(dragNode, endNode, dragPos, event) {
+            this.$emit('node-drag-end', dragNode, endNode, dragPos, event)
+        },
+        nodeDrop(dragNode, endNode, dragPos, event) {
+            this.$emit('node-drop', dragNode, endNode, dragPos, event)
+        }
+
     }
 }
 </script>
 
 <style scoped lang="scss">
+    .high-light-icon {
+        &.el-tree--highlight-current {
+            ::v-deep .el-tree-node.is-current > .el-tree-node__content {
+                .custom-tree-node {
+                    position: relative;
+
+                    &::after {
+                        content: '';
+                        display: block;
+                        width: 16px;
+                        height: 14px;
+                        position: absolute;
+                        right: -30px;
+                        top: 0;
+                        bottom: 0;
+                        margin: auto;
+                        background: url('./image/duihao.png') center center no-repeat;
+                        background-size: 100%;
+                    }
+                }
+            }
+        }
+    }
+
     .tree-line {
         width: 100%;
         height: auto;
@@ -157,21 +247,6 @@ export default {
             ::v-deep .el-tree-node.is-current > .el-tree-node__content {
                 background-color: #F0F7FF;
                 color: #2C90F3;
-                position: relative;
-
-                &::after {
-                    content: '';
-                    display: block;
-                    width: 16px;
-                    height: 14px;
-                    position: absolute;
-                    right: 0;
-                    top: 0;
-                    bottom: 0;
-                    margin: auto;
-                    background: url('./image/duihao.png') center center no-repeat;
-                    background-size: 100%;
-                }
             }
         }
 
@@ -179,7 +254,6 @@ export default {
 
             .el-tree-node__children {
                 position: relative;
-                padding-left: 20px;
 
                 &::before {
                     content: "";
@@ -195,19 +269,6 @@ export default {
 
             .el-tree-node__content {
                 height: 30px;
-                position: relative;
-
-                &::before {
-                    content: "";
-                    display: inline-block;
-                    position: absolute;
-                    left: -10px;
-                    top: 0;
-                    z-index: 1;
-                    width: 1px;
-                    height: 100%;
-                    border-left: 1px dashed #cdcdcd;
-                }
 
                 .el-tree-node__expand-icon {
                     margin-right: 10px;
@@ -221,11 +282,12 @@ export default {
                         content: "";
                         display: inline-block;
                         position: absolute;
-                        top: 50%;
-                        left: -52%;
-                        width: 50%;
-                        height: 1px;
-                        border-top: 1px dashed #cdcdcd;;
+                        top: -50%;
+                        left: -100%;
+                        width: 100%;
+                        height: 100%;
+                        border-bottom: 1px dashed #cdcdcd;
+                        border-left: 1px dashed #cdcdcd;
                     }
 
                     &::before {
@@ -237,15 +299,15 @@ export default {
                         display: inline-block;
                         width: 20px;
                         height: 20px;
-                        background-image: url(./image/metro.png);
-                        background-position: -126px 0px;
+                        background: url("./image/add.png") center center no-repeat;
+                        /*background-position: -126px 0px;*/
                     }
 
                     &.el-icon-caret-right.expanded {
                         transform: rotate(0);
 
                         &::before {
-                            background-position: -105px 0;
+                            background: url("./image/jian.png") center center no-repeat;
                         }
                     }
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -60,15 +60,18 @@
                 </div>
                 <div class="mw__sw-tree" v-if="curMenu === 'element-tree'">
                     <sw-tree
-                        :data="treeData"
+                        ref="swTree"
+                        :data="treeData1"
                         :show-line="true"
                         :show-checkbox="false"
+                        :show-select-icon="true"
                         :expand-on-click-node="false"
                         :highlight-current="true"
+                        draggable
                     >
-                        <!--                        <span slot-scope="{ node }">-->
-                        <!--                            <span>自定义{{ node.label }}</span>-->
-                        <!--                        </span>-->
+                        <span slot-scope="{ node }">
+                            <span>自定义-{{ node.label }}</span>
+                        </span>
                     </sw-tree>
                 </div>
             </el-main>
@@ -217,6 +220,268 @@ export default {
                 {
                     label: '一级 3',
                 }
+            ],
+            treeData1: [
+                {
+                    "children": [
+                        {
+                            "children": [
+                                {
+                                    "id": "12",
+                                    "label": "aa",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "14d32f323c3f393e7e7dc11c5c76ea36",
+                                    "label": "王企鹅",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "1670bff29aa91b9a8d0aa86c9b6b195b",
+                                    "label": "测试3",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "4673c6eee0c6300e2aca1294c4543c23",
+                                    "label": "我是",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "743bb731f861436c001997a2cdf2c97c",
+                                    "label": "熊",
+                                    "open": "false"
+                                }
+                            ],
+                            "label": "基础信息",
+                            "id": 201001,
+                            "open": "false"
+                        },
+                        {
+                            "children": [
+                                {
+                                    "id": "177f8165d40f50a4cc2b19c43c2f5f21",
+                                    "label": "noise",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "2df071bd6941adf7f58da29edba25fdb",
+                                    "label": "适宜2",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "3e7171061352e04ae4b01375d10008fd",
+                                    "label": "dsadasd",
+                                    "open": "false"
+                                }
+                            ],
+                            "label": "用能信息",
+                            "id": 201002,
+                            "open": "false"
+                        },
+                        {
+                            "children": [
+                                {
+                                    "id": "87caeabe45543f72f2498da8a2fa2fa5",
+                                    "label": "就没那么",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "8f40843bdb7239c31577154826b25a23",
+                                    "label": "新增1",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "aaf3f961834fd4e5b3ffef4caaa82a4d",
+                                    "label": "看看",
+                                    "open": "false"
+                                }
+                            ],
+                            "label": "能源控制系统",
+                            "id": 201003,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "用户需求",
+                            "id": 201004,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "我的",
+                            "id": 201006,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "试试",
+                            "id": 201007,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "最后",
+                            "id": 201007,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "饿",
+                            "id": 201009,
+                            "open": "false"
+                        }
+                    ],
+                    "label": "用户级调研",
+                    "id": 2010,
+                    "open": "false"
+                },
+                {
+                    "children": [
+                        {
+                            "children": [],
+                            "label": "采集系统",
+                            "id": 202001,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "配电系统",
+                            "id": 202002,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "暖通",
+                            "id": 202003,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "照明",
+                            "id": 202004,
+                            "open": "false"
+                        },
+                        {
+                            "children": [
+                                {
+                                    "id": "3b929734984d6bff30c71519c729a1ef",
+                                    "label": "光伏电站发电补贴电价",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "50a91e08943a670abece489dd2ac1090",
+                                    "label": "光伏电站面积",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "597ad106e5c0f7cf24363d0815eeaffc",
+                                    "label": "光伏电站投资预估单价",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "90524c84cc965d37923f221218caca8f",
+                                    "label": "光伏电站发电工商电价",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "e86e751fcad8568e3b6dc769312b80f3",
+                                    "label": "光伏电站所在地级市",
+                                    "open": "false"
+                                },
+                                {
+                                    "id": "e86e751fcad8568e3b6dc769312b80f4",
+                                    "label": "光伏电站所在地级市类别",
+                                    "open": "false"
+                                }
+                            ],
+                            "label": "光储充",
+                            "id": 202005,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "分布式能源",
+                            "id": 202006,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "给排水",
+                            "id": 202007,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "电梯",
+                            "id": 202008,
+                            "open": "false"
+                        }
+                    ],
+                    "label": "系统级调研",
+                    "id": 2020,
+                    "open": "false"
+                },
+                {
+                    "children": [
+                        {
+                            "children": [],
+                            "label": "采集系统",
+                            "id": 203001,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "配电系统",
+                            "id": 203002,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "暖通",
+                            "id": 203003,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "照明",
+                            "id": 203004,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "光储充",
+                            "id": 203005,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "分布式能源",
+                            "id": 203006,
+                            "open": "false"
+                        },
+                        {
+                            "children": [],
+                            "label": "给排水",
+                            "id": 203007,
+                            "open": "false"
+                        },
+                        {
+                            "children": [
+                                {
+                                    "label": "电梯1",
+                                    "id": 2030081,
+                                    "open": "false"
+                                }
+                            ],
+                            "label": "电梯",
+                            "id": 203008,
+                            "open": "false"
+                        }
+                    ],
+                    "label": "设备级调研",
+                    "id": 2030,
+                    "open": "false"
+                }
             ]
         }
     },
@@ -234,6 +499,9 @@ export default {
         },
         handleSelect(key) {
             this.curMenu = key
+            this.$nextTick(() => {
+                console.log(this.$refs.swTree)
+            })
         },
 
 
